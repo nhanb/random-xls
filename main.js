66e04034f4a97318e9b2e10a08a903a67b2ad507
Bùi Thành Nhân <hi@imnhan.com>
2021-06-16 19:54:02 +0700

    add Copy button

--- main.js
+++ main.js
@@ -2,171 +2,183 @@
 var choices = [];
 
 // Convenient loggin helper
-var logEl = document.getElementById('logs');
-var log = function(content) {
-    logEl.innerHTML += content.toString() + '\n';
-    logEl.scrollTop = logEl.scrollHeight;
+var logEl = document.getElementById("logs");
+var log = function (content) {
+  logEl.innerHTML += content.toString() + "\n";
+  logEl.scrollTop = logEl.scrollHeight;
 };
 
-var readWorkbook = function(workbook) {
-    choices = [];
-    var sheet_name_list = workbook.SheetNames;
-    var worksheet = workbook.Sheets.Sheet1;
-    for (var z in worksheet) {
-        switch (z[0]) {
-            case '!':
-                continue;
-            case 'A':  // name
-                choices.push({
-                    name: worksheet[z].v.toString(),
-                    picked: false,
-                });
-                break;
-            case 'B':  // group
-                choices[choices.length - 1].group = worksheet[z].v.toString();
-                break;
-            case 'C':
-                console.log('got', worksheet[z].v);
-                choices[choices.length - 1].priority = worksheet[z].v;
-        }
+var readWorkbook = function (workbook) {
+  choices = [];
+  var sheet_name_list = workbook.SheetNames;
+  var worksheet = workbook.Sheets.Sheet1;
+  for (var z in worksheet) {
+    switch (z[0]) {
+      case "!":
+        continue;
+      case "A": // name
+        choices.push({
+          name: worksheet[z].v.toString(),
+          picked: false,
+        });
+        break;
+      case "B": // group
+        choices[choices.length - 1].group = worksheet[z].v.toString();
+        break;
+      case "C":
+        console.log("got", worksheet[z].v);
+        choices[choices.length - 1].priority = worksheet[z].v;
     }
+  }
 
-    choices.forEach(function(choice) {
-        if (typeof choice.group === 'undefined') {
-            choice.group = '(không có nhóm)';
-        }
-    });
-
-    log('Vừa cập nhật ' + choices.length + ' tên.');
-
-    // Sort by priority
-    choices.sort(function(a, b) {
-      if (typeof a.priority === 'string') {
-        return 1;
-      } else if (typeof b.priority === 'string') {
-        return -1;
-      }
-      return a.priority - b.priority;
-    });
-
-    for (var aa in choices) {
-      console.log(choices[aa].priority);
+  choices.forEach(function (choice) {
+    if (typeof choice.group === "undefined") {
+      choice.group = "(không có nhóm)";
     }
-    console.log(choices);
+  });
 
-    updateGroupChoices();
+  log("Vừa cập nhật " + choices.length + " tên.");
+
+  // Sort by priority
+  choices.sort(function (a, b) {
+    if (typeof a.priority === "string") {
+      return 1;
+    } else if (typeof b.priority === "string") {
+      return -1;
+    }
+    return a.priority - b.priority;
+  });
+
+  for (var aa in choices) {
+    console.log(choices[aa].priority);
+  }
+  console.log(choices);
+
+  updateGroupChoices();
 };
 
 // Handle when user submits an excel file
-var handleFileInput = function(e) {
-    var f = this.files[0];
-    var reader = new FileReader();
-    var name = f.name;
-    reader.onload = function(e) {
-        var data = e.target.result;
+var handleFileInput = function (e) {
+  var f = this.files[0];
+  var reader = new FileReader();
+  var name = f.name;
+  reader.onload = function (e) {
+    var data = e.target.result;
 
-        /* if binary string, read with type 'binary' */
-        var wb = XLSX.read(data, {type: 'binary'});
+    /* if binary string, read with type 'binary' */
+    var wb = XLSX.read(data, { type: "binary" });
 
-        /* DO SOMETHING WITH workbook HERE */
-        readWorkbook(wb);
-    };
-    reader.readAsBinaryString(f);
+    /* DO SOMETHING WITH workbook HERE */
+    readWorkbook(wb);
+  };
+  reader.readAsBinaryString(f);
 };
-var inputEl = document.getElementById('fileInput');
-inputEl.addEventListener('change', handleFileInput, false);
+var inputEl = document.getElementById("fileInput");
+inputEl.addEventListener("change", handleFileInput, false);
 
 // List available groups as radio buttons
-var groupsEl = document.getElementById('groups');
-var updateGroupChoices = function() {
+var groupsEl = document.getElementById("groups");
+var updateGroupChoices = function () {
+  // Remove all child nodes a.k.a. choices
+  // This approach is much faster than setting innerHTML = ''
+  while (groupsEl.firstChild) {
+    groupsEl.removeChild(groupsEl.firstChild);
+  }
 
-    // Remove all child nodes a.k.a. choices
-    // This approach is much faster than setting innerHTML = ''
-    while (groupsEl.firstChild) {
-        groupsEl.removeChild(groupsEl.firstChild);
-    }
+  // Get a list of unique group choices
+  var groupChoices = choices.map(function (item) {
+    return item.group;
+  });
+  var uniqueGroupChoices = groupChoices.filter(function (el, pos) {
+    return groupChoices.indexOf(el) == pos;
+  });
 
-    // Get a list of unique group choices
-    var groupChoices = choices.map(function(item) {
-        return item.group;
-    });
-    var uniqueGroupChoices = groupChoices.filter(function(el, pos) {
-        return groupChoices.indexOf(el) == pos;
-    });
+  // Turn them into actual radio buttons
+  uniqueGroupChoices.map(function (item) {
+    var listItem = document.createElement("li");
 
-    // Turn them into actual radio buttons
-    uniqueGroupChoices.map(function(item) {
-        var listItem = document.createElement('li');
+    var radioInput = document.createElement("input");
+    radioInput.setAttribute("type", "radio");
+    radioInput.setAttribute("name", "group");
+    radioInput.setAttribute("id", item);
 
-        var radioInput = document.createElement('input');
-        radioInput.setAttribute('type', 'radio');
-        radioInput.setAttribute('name', 'group');
-        radioInput.setAttribute('id', item);
+    var label = document.createElement("label");
+    label.setAttribute("for", item);
+    label.innerHTML = item;
 
-        var label = document.createElement('label');
-        label.setAttribute('for', item);
-        label.innerHTML = item;
-
-        listItem.appendChild(radioInput);
-        listItem.appendChild(label);
-        groupsEl.appendChild(listItem);
-    });
-
-    // "Select all" choice
-    var listItem = document.createElement('li');
-    listItem.innerHTML = '<input type="radio" name="group" id="all"' +
-        ' checked="checked"><label for="all">Tất cả</label>';
+    listItem.appendChild(radioInput);
+    listItem.appendChild(label);
     groupsEl.appendChild(listItem);
+  });
+
+  // "Select all" choice
+  var listItem = document.createElement("li");
+  listItem.innerHTML =
+    '<input type="radio" name="group" id="all"' +
+    ' checked="checked"><label for="all">Tất cả</label>';
+  groupsEl.appendChild(listItem);
 };
 
 // Handle when user clicks "Pick Random Name" button
-var pickRandom = function(e) {
-    var choicesMatchingGroup;
-    var unpickedChoices;
-    var groupChoice = 'all';
-    var radios = document.getElementsByName('group');
+var pickRandom = function (e) {
+  var choicesMatchingGroup;
+  var unpickedChoices;
+  var groupChoice = "all";
+  var radios = document.getElementsByName("group");
 
-    for (var i = 0, length = radios.length; i < length; i++) {
-        if (radios[i].checked) {
-            groupChoice = radios[i].id;
-            break;
-        }
+  for (var i = 0, length = radios.length; i < length; i++) {
+    if (radios[i].checked) {
+      groupChoice = radios[i].id;
+      break;
     }
+  }
 
-    if (groupChoice !== 'all') {
-        choicesMatchingGroup = choices.filter(function(item) {
-            return item.group === groupChoice;
-        });
-    } else {
-        choicesMatchingGroup = choices;
-    }
-
-    unpickedChoices = choicesMatchingGroup.filter(function(item) {
-        return item.picked === false;
+  if (groupChoice !== "all") {
+    choicesMatchingGroup = choices.filter(function (item) {
+      return item.group === groupChoice;
     });
+  } else {
+    choicesMatchingGroup = choices;
+  }
 
-    var priorities = Array.from(new Set(unpickedChoices.map(function(item) {
-      return item.priority;
-    })));
-    console.log('p', priorities);
-    var currentPriority = priorities[0];
-    console.log('cp', currentPriority);
+  unpickedChoices = choicesMatchingGroup.filter(function (item) {
+    return item.picked === false;
+  });
 
-    var prioritizedChoices = unpickedChoices.filter(function(item) {
-        return item.priority === currentPriority;
-    })
-    console.log('prioritizedChoices:', prioritizedChoices);
+  var priorities = Array.from(
+    new Set(
+      unpickedChoices.map(function (item) {
+        return item.priority;
+      })
+    )
+  );
+  console.log("p", priorities);
+  var currentPriority = priorities[0];
+  console.log("cp", currentPriority);
 
-    var index = Math.floor(Math.random()*prioritizedChoices.length);
-    var item = unpickedChoices[index];
-    item.picked = true;
-    log('=> ' + item.name + ' - ' + item.group);
+  var prioritizedChoices = unpickedChoices.filter(function (item) {
+    return item.priority === currentPriority;
+  });
+  console.log("prioritizedChoices:", prioritizedChoices);
 
-    if (unpickedChoices.length === 1) {
-        log('Hết dữ liệu. Lặp lại...');
-        for (var i = 0; i < choicesMatchingGroup.length; i++) {
-            choicesMatchingGroup[i].picked = false;
-        }
+  var index = Math.floor(Math.random() * prioritizedChoices.length);
+  var item = unpickedChoices[index];
+  item.picked = true;
+  log("=> " + item.name + " - " + item.group);
+
+  if (unpickedChoices.length === 1) {
+    log("Hết dữ liệu. Lặp lại...");
+    for (var i = 0; i < choicesMatchingGroup.length; i++) {
+      choicesMatchingGroup[i].picked = false;
     }
+  }
 };
-document.getElementById('pick-btn').onclick = pickRandom;
+
+function copyResults() {
+  var copyText = document.querySelector("#logs");
+  copyText.select();
+  document.execCommand("copy");
+  alert("Đã copy!");
+}
+document.getElementById("pick-btn").onclick = pickRandom;
+document.getElementById("copy-btn").onclick = copyResults;
